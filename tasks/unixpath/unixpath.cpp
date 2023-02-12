22b226c72356ec1db285102afa9d5e6d9cc7555b
Kirill Troshkin <Skykirill@inbox.ru>
2023-02-12 14:29:49 +0300

    Sending unixpath

--- tasks/unixpath/unixpath.cpp
+++ tasks/unixpath/unixpath.cpp
@@ -1,5 +1,56 @@
 #include "unixpath.h"
+#include <vector>
+
+std::vector<std::string_view> Splitted(std::string_view s) {
+    std::vector<std::string_view> splitted_str;
+    if (s[0] == '/') {
+        size_t divider = 0;
+        while (s.find('/', divider + 1) != std::string::npos) {
+            if (!s.substr(divider + 1, s.find('/', divider + 1) - divider - 1).empty()) {
+                splitted_str.emplace_back(s.substr(divider + 1, s.find('/', divider + 1) - divider - 1));
+            }
+            divider = s.find('/', divider + 1);
+        }
+        if (!s.substr(divider + 1, s.find('/', divider + 1) - divider - 1).empty()) {
+            splitted_str.emplace_back(s.substr(divider + 1, s.size() - divider - 1));
+        }
+    } else {
+        splitted_str.emplace_back(s.substr(0, s.find('/')));
+        size_t divider = s.find('/');
+        while (s.find('/', divider + 1) != std::string::npos) {
+            if (!s.substr(divider + 1, s.find('/', divider + 1) - divider - 1).empty()) {
+                splitted_str.emplace_back(s.substr(divider + 1, s.find('/', divider + 1) - divider - 1));
+            }
+            divider = s.find('/', divider + 1);
+        }
+        if (!s.substr(divider + 1, s.find('/', divider + 1) - divider - 1).empty()) {
+            splitted_str.emplace_back(s.substr(divider + 1, s.size() - divider - 1));
+        }
+    }
+    return splitted_str;
+}
 
 std::string NormalizePath(std::string_view current_working_dir, std::string_view path) {
-    return "";
+    std::string normalizedpath;
+    std::vector<std::string_view> vector_of_words;
+    for (auto word : Splitted(current_working_dir)) {
+        vector_of_words.push_back(word);
+    }
+    for (auto word : Splitted(path)) {
+        if (word == static_cast<std::string_view>("..")) {
+            if (!vector_of_words.empty()) {
+                vector_of_words.pop_back();
+            }
+        } else if (word != static_cast<std::string_view>(".")) {
+            vector_of_words.push_back(word);
+        }
+    }
+    for (auto word : vector_of_words) {
+        normalizedpath += "/";
+        normalizedpath += static_cast<std::string>(word);
+    }
+    if (normalizedpath.empty()) {
+        normalizedpath += "/";
+    }
+    return normalizedpath;
 }
