d63781b3489718e28288a8a6ed9d2efc14311ef9
Kirill Troshkin <Skykirill@inbox.ru>
2023-02-12 15:53:32 +0300

    Changed unixpath due to new understanding

--- tasks/unixpath/unixpath.cpp
+++ tasks/unixpath/unixpath.cpp
@@ -33,30 +33,49 @@ std::vector<std::string_view> Splitted(std::string_view s) {
 std::string NormalizePath(std::string_view current_working_dir, std::string_view path) {
     std::string normalizedpath;
     std::vector<std::string_view> vector_of_words;
-    for (auto word : Splitted(current_working_dir)) {
-        if (word == static_cast<std::string_view>("..")) {
-            if (!vector_of_words.empty()) {
-                vector_of_words.pop_back();
+    if (path[0] != '/') {
+        for (auto word : Splitted(current_working_dir)) {
+            if (word == static_cast<std::string_view>("..")) {
+                if (!vector_of_words.empty()) {
+                    vector_of_words.pop_back();
+                }
+            } else if (word != static_cast<std::string_view>(".")) {
+                vector_of_words.push_back(word);
             }
-        } else if (word != static_cast<std::string_view>(".")) {
-            vector_of_words.push_back(word);
         }
-    }
-    for (auto word : Splitted(path)) {
-        if (word == static_cast<std::string_view>("..")) {
-            if (!vector_of_words.empty()) {
-                vector_of_words.pop_back();
+        for (auto word : Splitted(path)) {
+            if (word == static_cast<std::string_view>("..")) {
+                if (!vector_of_words.empty()) {
+                    vector_of_words.pop_back();
+                }
+            } else if (word != static_cast<std::string_view>(".")) {
+                vector_of_words.push_back(word);
             }
-        } else if (word != static_cast<std::string_view>(".")) {
-            vector_of_words.push_back(word);
         }
-    }
-    for (auto word : vector_of_words) {
-        normalizedpath += "/";
-        normalizedpath += static_cast<std::string>(word);
-    }
-    if (normalizedpath.empty()) {
-        normalizedpath += "/";
+        for (auto word : vector_of_words) {
+            normalizedpath += "/";
+            normalizedpath += static_cast<std::string>(word);
+        }
+        if (normalizedpath.empty()) {
+            normalizedpath += "/";
+        }
+    } else {
+        for (auto word : Splitted(path)) {
+            if (word == static_cast<std::string_view>("..")) {
+                if (!vector_of_words.empty()) {
+                    vector_of_words.pop_back();
+                }
+            } else if (word != static_cast<std::string_view>(".")) {
+                vector_of_words.push_back(word);
+            }
+        }
+        for (auto word : vector_of_words) {
+            normalizedpath += "/";
+            normalizedpath += static_cast<std::string>(word);
+        }
+        if (normalizedpath.empty()) {
+            normalizedpath += "/";
+        }
     }
     return normalizedpath;
 }
