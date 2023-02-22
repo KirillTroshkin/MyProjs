047ea5515d6f66f9c2675c5cd483ffd58f0a6ad1
Kirill Troshkin <Skykirill@inbox.ru>
2023-02-23 00:01:28 +0300

    Sending scorer2

--- tasks/scorer2/scorer.h
+++ tasks/scorer2/scorer.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "../scorer/scorer.h"
 #include <map>
 #include <set>
 #include <string>
@@ -10,6 +11,13 @@ using TaskName = std::string;
 
 using ScoreTable = std::map<StudentName, std::set<TaskName>>;
 
+struct TaskInf {
+    StudentName student_name;
+    TaskName task_name;
+    bool checking_approved = false;
+    bool request_merged = true;
+};
+
 class Scorer {
 public:
     void OnCheckFailed(const StudentName& student_name, const TaskName& task_name);
@@ -17,6 +25,9 @@ public:
     void OnMergeRequestOpen(const StudentName& student_name, const TaskName& task_name);
     void OnMergeRequestClosed(const StudentName& student_name, const TaskName& task_name);
     void Reset();
+    std::map<StudentName, std::map<TaskName, TaskInf>> checking_results;
+    std::map<StudentName, std::set<TaskName>> results;
+    ScoreTable StudentsResults;
 
     ScoreTable GetScoreTable() const;
 };
